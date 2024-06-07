bf47dbdb330ffcf4476cbb15bc112a9f0830620c
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-06-07 13:10:57 -0400

    Spring boot starter auto configuration and spring boot starter devtools

--- src/main/java/com/springboot/learn_spring_boot/CourseController.java
+++ src/main/java/com/springboot/learn_spring_boot/CourseController.java
@@ -13,7 +13,9 @@ public class CourseController {
 	public List<Course> retrieveAllCourses() {
 		return Arrays.asList(
 				new Course(1, "Spring framework", "Sindhu"),
-				new Course(2, "Spring boot", "Sindhu")
+				new Course(2, "Spring boot", "Sindhu"),
+				new Course(3, "Spring dev tools", "Sindhu"),
+				new Course(4, "Spring dev tools", "Sindhu")
 				);
 	}
 }
